7ff99f134a26dae74c6a9c1cac8fc71efe4589ce
Gedulis120 <gediminas.repecka@gmail.com>
2021-04-13 18:37:55 +0300

    update

---
+++ Automate booring stuff with python/Ch 4 Lists/15_Picture_grid.py
@@ -0,0 +1,26 @@
+grid = [['.','.','.','.','.','.'],
+        ['.','0','0','.','.','.'],
+        ['0','0','0','0','.','.'],
+        ['0','0','0','0','0','.'],
+        ['.','0','0','0','0','0'],
+        ['0','0','0','0','0','.'],
+        ['0','0','0','0','.','.'],
+        ['.','0','0','.','.','.'],
+        ['.','.','.','.','.','.']]
+
+grid_rows = len(grid)
+grid_row = 0
+
+#for i in range(grid_rows):
+#    #print(len(grid[grid_row])) 6 6 6 6 6 6 6
+#    for i in range(len(grid[grid_row])):
+#        print(grid[i][grid_row], end='')
+#    grid_row += 1
+#    print()
+
+for i in range(len(grid[grid_row])):
+    #print(len(grid[grid_row])) 6 6 6 6 6 6 6
+    for i in range(grid_rows):
+        print(grid[i][grid_row], end='')
+    grid_row += 1
+    print()
