504339255cbc9ede93cde03ccd66bd29a6db9d0d
Gedulis120 <gediminas.repecka@gmail.com>
2021-04-13 18:42:43 +0300

    update

--- Automate booring stuff with python/Ch 4 Lists/15_Picture_grid.py
+++ Automate booring stuff with python/Ch 4 Lists/15_Picture_grid.py
@@ -11,12 +11,15 @@ grid = [['.','.','.','.','.','.'],
 grid_rows = len(grid)
 grid_row = 0
 
+#At first i tried this, but it didn't work:
 #for i in range(grid_rows):
-#    #print(len(grid[grid_row])) 6 6 6 6 6 6 6
+#    #print(len(grid[grid_row])) (returns 6 6 6 6 6 6 6)
 #    for i in range(len(grid[grid_row])):
 #        print(grid[i][grid_row], end='')
 #    grid_row += 1
 #    print()
+
+#Than i tried this and it DID work:
 
 for i in range(len(grid[grid_row])):
     #print(len(grid[grid_row])) 6 6 6 6 6 6 6
